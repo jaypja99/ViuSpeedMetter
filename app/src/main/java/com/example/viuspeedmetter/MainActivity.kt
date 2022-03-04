62bd7e972f6ac914b3682d72d6ecda66a26613db
jaypja99 <jaypja99@gmail.com>
2022-03-04 10:41:09 +0530

    Complted

--- app/src/main/java/com/example/viuspeedmetter/MainActivity.kt
+++ app/src/main/java/com/example/viuspeedmetter/MainActivity.kt
@@ -10,9 +10,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        val right_gauge=findViewById<View>(R.id.right_gauge)
         val left_gauge=findViewById<View>(R.id.left_gauge)
+        val left_teco_gauge=findViewById<View>(R.id.left_teco_black)
+        val right_gauge=findViewById<View>(R.id.right_blue_gauge)
+        val right_teco_gauge=findViewById<View>(R.id.right_gauge)
 
-        left_gauge.animate().translationY(-1300f).duration=30000
+        val imgDots=findViewById<View>(R.id.imgDots)
+
+        left_gauge.animate().translationY(-415f).duration=10000
+        left_teco_gauge.animate().translationY(-415f).duration=10000
+
+        right_gauge.animate().translationY(-415f).duration=10000
+        right_teco_gauge.animate().translationY(-415f).duration=10000
+
+        imgDots.animate().scaleX(1.1f).scaleY(1.1f).duration=7000
     }
 }
